0f75f106e52bd93e9997822ca14c2a0345e2a9c7
ExTeeP <54740245+ExTeeP@users.noreply.github.com>
2019-10-26 09:58:25 +0300

    modal overlay

--- js/script-catalog.js
+++ js/script-catalog.js
@@ -3,13 +3,21 @@
 var buttonBuy = document.querySelector(".button-buy"); //находит 1 ссылку списка, при querySelectorAll выдает ошибку
 var popupNotify = document.querySelector(".modal-notify");
 var buttonClose = document.querySelector(".button-close");
+var closeModal = document.querySelector(".overlay");
 
 buttonBuy.addEventListener("click", function (evt) {
   evt.preventDefault();
   popupNotify.classList.add("modal-notify-show");
+  closeModal.classList.add("overlay-show");
 });
 
 buttonClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   popupNotify.classList.remove("modal-notify-show");
+  closeModal.classList.remove("overlay-show");
 });
+
+closeModal.addEventListener("click", function(evt) {
+  popupNotify.classList.remove("modal-notify-show");
+  closeModal.classList.remove("overlay-show");
+})
--- js/script-index.js
+++ js/script-index.js
@@ -3,13 +3,23 @@
 var buttonContact = document.querySelector(".button-contact");
 var popupFeedback = document.querySelector(".modal-feedback");
 var buttonClose = document.querySelector(".button-close.close-form");
+var closeModal = document.querySelector(".overlay");
 
 buttonContact.addEventListener("click", function (evt) {
   evt.preventDefault();
   popupFeedback.classList.add("modal-feedback-show");
+  closeModal.classList.add("overlay-show");
 });
 
 buttonClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   popupFeedback.classList.remove("modal-feedback-show");
+  closeModal.classList.remove("overlay-show");
 });
+
+closeModal.addEventListener("click", function(evt) {
+  popupFeedback.classList.remove("modal-feedback-show");
+  closeModal.classList.remove("overlay-show");
+})
+
+//modal overlay
